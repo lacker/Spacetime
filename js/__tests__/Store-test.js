59b35628a85cd4ef23044b58560e17ffff9a340b
Kevin Lacker <lacker@gmail.com>
2015-10-24 14:12:32 -0700

    test the construction of the game

--- js/__tests__/Store-test.js
+++ js/__tests__/Store-test.js
@@ -2,8 +2,27 @@
 
 jest.autoMockOff();
 
+let Store = require('../Store');
+
+// Applies actions to a new store
+function run(actions) {
+  let s = Store();
+  for (let action of actions) {
+    s.dispatch(action);
+  }
+  return s.getState();
+}
+
 describe('Store', () => {
- it('can be constructed', () => {
-   let Store = require('../Store')();
+ it('can start a game', () => {
+   let s = run([{
+     type: 'startGame',
+     players: ['alice', 'bob'],
+   }]);
+   expect(s.turn).toEqual('alice');
+   expect(s.hand.size).toEqual(2);
+   expect(s.board.size).toEqual(2);
+   expect(s.life.size).toEqual(2);
+   expect(s.players.size).toEqual(2);
  });
 });
